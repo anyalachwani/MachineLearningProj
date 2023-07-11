46e8e62bb86d870e8eecf1039b209a02125c202b
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-07-10 20:22:49 -0700

    Address PEP8 warnings, fix no-header bug

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 - Install Python version 3.8 or above [here](https://www.python.org).
 - Install dependencies: `pip3 install scikit-learn matplotlib tqdm`
+- Download the dataset from [here](https://www.kaggle.com/datasets/andrewmvd/heart-failure-clinical-data)
 - Run main script: `python3 main.py`
--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 import numpy as np
-from io import StringIO
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LinearRegression
-f = open("/Users/anyalachwani/Downloads/heart_failure_clinical_records_dataset.csv", "r")
-data = f.read()
-input_data = np.genfromtxt(StringIO(data), delimiter=",", dtype='float64',usecols=np.arange(0,11))
-output_data = np.genfromtxt(StringIO(data), delimiter=",", dtype='float64',usecols=(12))
-X_train, X_test, y_train, y_test = train_test_split(input_data, output_data, test_size=0.2, random_state=42)
 
-model = LogisticRegression()
-model.fit(X_train, y_train)
+def main():
+    filename = 'heart_failure_clinical_records_dataset.csv'
+    input_data = np.genfromtxt(filename, delimiter=',', skip_header=1, dtype='float64', usecols=np.arange(0, 11))
+    output_data = np.genfromtxt(filename, delimiter=',', skip_header=1, dtype='float64', usecols=(12))
+    X_train, X_test, y_train, y_test = train_test_split(input_data, output_data, test_size=0.2, random_state=42)
 
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy of test data:", accuracy)
+    test_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, test_pred)
+    print('Accuracy of test data:', accuracy)
 
-y_pred1 = model.predict(X_train)
-accuracy1 = accuracy_score(y_train, y_pred1)
-print("Accuracy of training data: ", accuracy1)
+    train_pred = model.predict(X_train)
+    accuracy1 = accuracy_score(y_train, train_pred)
+    print('Accuracy of training data: ', accuracy1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    main()
