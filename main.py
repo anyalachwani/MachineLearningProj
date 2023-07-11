8050005a4eda9dc91ffd88f9b5e63d683e0b7bb5
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-07-10 20:46:19 -0700

    Use MLPClassifier instead of LR, rename variables

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
 def main():
     filename = 'heart_failure_clinical_records_dataset.csv'
     input_data = np.genfromtxt(filename, delimiter=',', skip_header=1, dtype='float64', usecols=np.arange(0, 11))
     output_data = np.genfromtxt(filename, delimiter=',', skip_header=1, dtype='float64', usecols=(12))
-    X_train, X_test, y_train, y_test = train_test_split(input_data, output_data, test_size=0.2, random_state=42)
+    inputs_train, inputs_dev, target_train, target_dev = train_test_split(input_data, output_data, test_size=0.2, random_state=42)
 
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
+    model = MLPClassifier()
+    model.fit(inputs_train, target_train)
 
-    test_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, test_pred)
-    print('Accuracy of test data:', accuracy)
+    predictions_dev = model.predict(inputs_dev)
+    print('Accuracy of test data:', accuracy_score(target_dev, predictions_dev))
 
-    train_pred = model.predict(X_train)
-    accuracy1 = accuracy_score(y_train, train_pred)
-    print('Accuracy of training data: ', accuracy1)
+    predictions_train = model.predict(inputs_train)
+    print('Accuracy of training data: ', accuracy_score(target_train, predictions_train))
 
 if __name__ == '__main__':
     main()
